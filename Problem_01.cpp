d2e796c4ca6c79a1687398e467cd04c1425e47ab
SantanuKGP <Santanuk13@gmail.com>
2023-01-02 00:23:48 +0530

    02-01-2023

---
+++ Problem_01.cpp
@@ -0,0 +1,18 @@
+/*
+author : santanukgp
+problem:https://codeforces.com/problemset/problem/{ProblemSet}/{ProblemNumber}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	if(sizeof(int)>-1) cout<<"Yes\n";
+	else cout<<"No\n";
+	/*
+	In C++, when an integer value is compared with an unsigned it,
+	the int is promoted to unsigned. Negative numbers are stored in 2's complement 
+	form and unsigned value of the 2's complement form is much higher than the sizeof int.
+	*/
+	return 0;
+}
